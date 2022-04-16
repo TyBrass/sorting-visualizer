89d906573f73a3465519ebd167c35c8d0d554579
Ty Brass <tybrass@outlook.com>
2022-04-16 13:21:22 -0500

    Added selectors and some initial functions to JS. I don't want to get too ahead of myself, but I think this is going to be easier than I thought.

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,66 @@
+"use strict"
 
-// Get option values
+// ==== Set up page =================================================
 const arraySizeInput = document.querySelector('#array-size-input');
 const sortSelector = document.querySelector('#sort-select');
-let arraySize = arraySizeInput.value;
-let sortSelection = sortSelector.value;
-
+const newArrayBtn = document.querySelector('#new-array-btn');
 const graphContainer = document.querySelector('#graph');
+
+newArrayBtn.addEventListener("click", function() {
+  arraySize = arraySizeInput.value;
+  sortSelection = sortSelector.value;
+  initializeArray(arraySize);
+});
+
+// Array contains integers, sorted by those values, and the value corresponds 
+// to the element's height in pixels (1-600 inclusive).
+let arrayArray = [];  // Array representing the on-screen array
+// ==================================================================
+
+// ==== Misc. =======================================================
+function initializeArray(size) {
+  arrayArray = [];
+  for (let i = 0; i < size; i++)
+    arrayArray.push(Math.floor(Math.random() * 600) + 1);
+}
+
+
+// Could we instead an event listener to the array being updated?
+function updateGraph() {
+
+}
+// ==================================================================
+
+// ==== Sorting methods and helpers =================================
+function quickSort() {
+
+}
+
+function partition() {
+
+}
+
+function mergeSort() {
+
+}
+
+function merge() {
+
+}
+
+function heapSort() {
+
+}
+
+function heapify() {
+
+}
+
+function insertionSort() {
+
+}
+
+function bubbleSort() {
+  
+}
+// ==================================================================
