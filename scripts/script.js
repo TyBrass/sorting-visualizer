386d3b1f8e26c0334756dc1dbdbd698ae023de3e
Ty Brass <tybrass@outlook.com>
2022-04-16 12:38:35 -0500

    Tried to do some mobile responsiveness, cancelled that and leaving it for later. Adding some JS, changed some HTML.

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+
+// Get option values
+const arraySizeInput = document.querySelector('#array-size-input');
+const sortSelector = document.querySelector('#sort-select');
+let arraySize = arraySizeInput.value;
+let sortSelection = sortSelector.value;
+
+const graphContainer = document.querySelector('#graph');
